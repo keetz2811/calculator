9e4eb5208c2d2bfee8b5eef9f0f1c8c96d7e751b
keetz2811 <65348604+keetz2811@users.noreply.github.com>
2021-03-23 23:40:06 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser=require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/",function(req,res){
+var num1 = Number(req.body.num1);
+var num2 = Number(req.body.num2);
+var result = num+num2;
+res.send("the result is " + result);
+});
+
+app.get("/bmiCalculator",function(req,res){
+   res.sendFile(__dirname + "/bmiCalculator.html");
+ });
+ app.post("/bmiCalculator",function(req,res){
+   var weight = parseFloat(req.body.weight);
+   var height = parseFloat(req.body.height);
+   var result = weight / (height * height);
+   res.send(" the bmi of the person is " + result );
+});
+ app.listen(3000,function(){
+   console.log("starting at port 3000");
+ });
